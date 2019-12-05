a60eea5331e334575f5b13bad99662f866dd8894
currypat <currypat1985@gmail.com>
2019-12-05 10:09:09 +0100

    yeah

--- knots/urls.py
+++ knots/urls.py
@@ -1,15 +1,11 @@
 from django.urls import path
 
-from knots.views import KnotCreate, KnotDetail, KnotList, KnotDelete
-from knots.views import AlternativeNameCreate
+from knots.views import KnotDetail, KnotList
 
 app_name = 'knots'
 
 urlpatterns = [
     path('', KnotList.as_view(), name='knot-list'),
-    path('create/', KnotCreate.as_view(), name='knot-create'),
     path('<slug:slug>/', KnotDetail.as_view(), name='knot-detail'),
-    path('<slug:slug>/delete/', KnotDelete.as_view(), name='knot-delete'),
-    path('<slug:slug>/add-name/', AlternativeNameCreate.as_view(), name='add-name'),
 ]
 
