cf45c019f36a3517fd09325829abd2225c434bdc
currypat <currypat1985@gmail.com>
2019-12-05 10:07:48 +0100

    create update delete operations all through admin site

--- knots/admin.py
+++ knots/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from knots.models import Knot, AlternativeName
+
+class AlternativeNameInline(admin.TabularInline):
+    model = AlternativeName
+
+class KnotAdmin(admin.ModelAdmin):
+    inlines = [
+        AlternativeNameInline,
+    ]
+
+admin.site.register(Knot, KnotAdmin)
+admin.site.register(AlternativeName)
--- knots/views.py
+++ knots/views.py
@@ -1,5 +1,4 @@
 from django.views.generic import DetailView, ListView   
-from django.views.generic.edit import CreateView, DeleteView
 from django.urls import reverse
 
 from knots.models import Knot, AlternativeName
@@ -11,34 +10,3 @@ class KnotDetail(DetailView):
 
 class KnotList(ListView):
     model = Knot
-
-
-class KnotCreate(CreateView):
-    model = Knot
-    fields = ['name']
-
-
-class KnotDelete(DeleteView):
-    model = Knot
-    success_url = '/knots/' 
-
-
-class AlternativeNameCreate(CreateView):
-    model = AlternativeName
-    fields = ['name']
-
-    def dispatch(self, request, *args, **kwargs):
-        self.knot = Knot.objects.get(slug=self.kwargs.get('slug'))
-        return super().dispatch(request, *args, **kwargs)
-
-    def form_valid(self, form):
-        form.save(commit=False)
-        form.instance.knot = self.knot
-        return super(AlternativeNameCreate, self).form_valid(form)
-
-    def get_success_url(self):
-        return reverse('knots:knot-detail', kwargs={'slug': self.knot.slug})
-
-
-#class AlternativeNameDelete(DeleteView):
-###    model = AlternativeName
